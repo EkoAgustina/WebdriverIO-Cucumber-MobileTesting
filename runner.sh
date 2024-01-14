dab8853e8303271492733bd5d0e021d4646ef801
EkoAgustina <eko.agustina86@gmail.com>
2024-01-14 09:26:07 +0700

    Updated the local runner configuration

--- runner.sh
+++ runner.sh
@@ -1,21 +1,56 @@
 platform=${1}
 
-# Android setup
-# apps='swaglabs.apk'
-# deviceName='Pixel4_12'
-
-# iOS setup
 apps='swaglabs.app'
-deviceName='iPhone_15'
+
+# Setup for android platform
+androidName='Pixel4_12'
+
+# Setup for iOS platforms
+iosName='iPhone_15'
 iosUdid='EC865F6C-9547-4290-9942-B2A83F91B81C'
 
-#Cucumber tags
+# Cucumber Tags
 cucumberTags='@invalidAccount'
 
+
+#############################################################
+if [ -z "$apps" ]; then
+    echo "Error: 'apps' cannot be empty!"
+    exit 1
+fi
+if [ -z "$cucumberTags" ]; then
+    echo "Error: 'cucumberTags' cannot be empty!"
+    exit 1
+fi
+
+# Check based on the platform
+if [ "$platform" == "android" ]; then
+    # Check if androidName is not empty
+    if [ -z "$androidName" ]; then
+        echo "Error: 'androidName' cannot be empty for Android platform!"
+        exit 1
+    fi
+elif [ "$platform" == "ios" ]; then
+    # Check if iosName and iosUdid are not empty
+    if [ -z "$iosName" ]; then
+        echo "Error: 'iosName' cannot be empty for iOS platform!"
+        exit 1
+    fi
+    if [ -z "$iosUdid" ]; then
+        echo "Error: 'iosUdid' cannot be empty for iOS platform!"
+        exit 1
+    fi
+else
+    echo "Error: Unsupported platform. Please provide 'android' or 'ios'."
+    exit 1
+fi
+#############################################################
+
 export platformType=${platform}
-export deviceName=${deviceName}
 export apps=${apps}
+export androidName=${androidName}
+export iosName=${iosName}
 export iosUdid=${iosUdid}
 export cucumberTags=${cucumberTags}
 
-npm run wdio
+yarn test
